d79dbcd55694f005f0be2cce02fe789b91978dd0
Sachinpaudel <sachinpaudel32@gmail.com>
2026-06-22 17:34:21 +0545

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,10 @@
+async function getData() {
+    try {
+        const response = await fetch("https://api.example.com");
+        const data = await response.json();
 
+        console.log(data);
+    } catch (error) {
+        console.log("Error:", error);
+    }
+}
